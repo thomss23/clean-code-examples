37628bad6098f54b0a6f66237f5fdfeadb5952ab
Mihaela-Diana Fagateanu <mihaela-diana.fagateanu1@ing.com>
2023-03-24 18:40:11 +0200

    refactoring The Step-down Rule

--- src/main/java/com/demo/cleancode/examples/functions/TopDownNarrativeGood.java
+++ src/main/java/com/demo/cleancode/examples/functions/TopDownNarrativeGood.java
@@ -1,20 +1,37 @@
 package com.demo.cleancode.examples.functions;
 
+import com.demo.cleancode.examples.util.service.FryingPanService;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * The Step-down Rule.
+ * We want the code to read like a top-down narrative.
+ * We want every function to be followed by those at the next level of abstraction so that we can read the program, descending one level of abstraction at a time as we read down the list of functions.
+ * It implies that the function ordering is no longer random. A caller function should always reside above the callee function.
+ **/
+@RequiredArgsConstructor
 public class TopDownNarrativeGood {
 
-    void method1() {
-        method2();
-        method4();
+    private final FryingPanService fryingPanService;
+
+    public void makeBreakfast() {
+        addEggs();
+        cook();
+        serve();
     }
 
-    void method2() {
-        method3();
+    private void addEggs() {
+        System.out.println("Add eggs from the fridge");
+
     }
 
-    void method3() {
-        method4();
+    private void cook() {
+        fryingPanService.mixContents();
+        fryingPanService.addCondiments();
+        fryingPanService.mixContents();
     }
-    void method4() {
 
+    private void serve() {
+        System.out.println("Serve breakfast");
     }
 }
