61b1277c4330a9b5a9e00eb9d2046d2828a60b20
Mihaela-Diana Fagateanu <mihaela-diana.fagateanu1@ing.com>
2023-04-24 11:19:43 +0300

    small refactoring

--- src/main/java/com/demo/cleancode/examples/functions/SwitchStatementBad.java
+++ src/main/java/com/demo/cleancode/examples/functions/SwitchStatementBad.java
@@ -5,6 +5,8 @@ import com.demo.cleancode.examples.util.exception.InvalidEmployeeTypeException;
 import com.demo.cleancode.examples.util.model.Employee;
 import com.demo.cleancode.examples.util.model.Money;
 
+import java.util.Date;
+
 public class SwitchStatementBad {
 
 //    Main problems with the below function:
@@ -13,35 +15,7 @@ public class SwitchStatementBad {
 //    - it violates the Single Responsibility Principle (SRP) because there is more than one reason for it to change
 //    - it violates the Open Closed Principle (OCP) because it must change whenever new types are added.
 //    - imagine what it would happen is an extra country type would be added (in RO, for instance, the work week has 40 hours, in NL 36 hours etc)
-//    - there are an unlimited number of other functions that will have the same structure:
-//
-//    boolean isPayDay(Employee employee, Date date) {
-//        if (isEndOfMonthDate(date)) {
-//            switch (employee.getType()) {
-//                case COMMISSIONED:
-//                    return false;
-//                case HOURLY:
-//                    return false;
-//                case SALARIED:
-//                    return true;
-//                default:
-//                    throw new InvalidEmployeeTypeException(employee.getType());
-//            }
-//        }
-//        if (isDateBetweenTheFirst10DaysOfMonth(date)) {
-//            switch (employee.getType()) {
-//                case COMMISSIONED:
-//                    return true;
-//                case HOURLY:
-//                    return false;
-//                case SALARIED:
-//                    return false;
-//                default:
-//                    throw new InvalidEmployeeTypeException(employee.getType());
-//            }
-//        }
-//    etc
-//    }
+//    - there are an unlimited number of other functions that will have the same structure, like isPayDay() for instance
 
     public Money calculatePay(Employee employee) throws InvalidEmployeeTypeException {
         switch (employee.getType()) {
@@ -54,6 +28,34 @@ public class SwitchStatementBad {
             default:
                 throw new InvalidEmployeeTypeException(employee.getType());
         }
+    }
+
+    public boolean isPayDay(Employee employee, Date date) {
+        if (isEndOfMonthDate(date)) {
+            switch (employee.getType()) {
+                case COMMISSIONED:
+                    return false;
+                case HOURLY:
+                    return false;
+                case SALARIED:
+                    return true;
+                default:
+                    throw new InvalidEmployeeTypeException(employee.getType());
+            }
+        }
+        if (isDateBetweenTheFirst10DaysOfMonth(date)) {
+            switch (employee.getType()) {
+                case COMMISSIONED:
+                    return true;
+                case HOURLY:
+                    return false;
+                case SALARIED:
+                    return false;
+                default:
+                    throw new InvalidEmployeeTypeException(employee.getType());
+            }
+        }
+        return false;
     }
 
     private Money calculateCommissionedPay(Employee employee) {
@@ -69,6 +71,14 @@ public class SwitchStatementBad {
     private Money calculateSalariedPay(Employee employee) {
         //do some logic with employee and calculate salary regularly
         return new Money();
+    }
+
+    private boolean isDateBetweenTheFirst10DaysOfMonth(Date date) {
+        return false;
+    }
+
+    private boolean isEndOfMonthDate(Date date) {
+        return false;
     }
 }
 
