9273a38b03dd3d8f40d7c53aa0a17a3b9204a223
Mihaela-Diana Fagateanu <mihaela-diana.fagateanu1@ing.com>
2023-05-05 19:06:09 +0300

    use lombok

--- src/main/java/com/demo/cleancode/examples/util/model/EmployeeInfo.java
+++ src/main/java/com/demo/cleancode/examples/util/model/EmployeeInfo.java
@@ -1,37 +1,17 @@
 package com.demo.cleancode.examples.util.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeInfo {
-    private final String name;
-    private final String type;
-    private final int age;
-    private final String color;
-    private final String address;
 
-    public EmployeeInfo(String name, String type, int age, String color, String address) {
-        this.name = name;
-        this.type = type;
-        this.age = age;
-        this.color = color;
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public String getAddress() {
-        return address;
-    }
+    private String name;
+    private String type;
+    private int age;
+    private String color;
+    private String address;
 }
