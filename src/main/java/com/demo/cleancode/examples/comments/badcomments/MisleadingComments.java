c4742758aaadb9efd450922c6301d91b5e58dd7c
[Thomas-Alexandru Borbeli] <thomas-alexandru.borbeli@ing.com>
2023-05-08 11:19:25 +0300

    provide better example for misleading comments

--- src/main/java/com/demo/cleancode/examples/comments/badcomments/MisleadingComments.java
+++ src/main/java/com/demo/cleancode/examples/comments/badcomments/MisleadingComments.java
@@ -1,17 +1,31 @@
 package com.demo.cleancode.examples.comments.badcomments;
 
 /*
-In this code, the comment "This function calculates the average of the given array" is misleading,
-as the method actually calculates the integer division of the sum of the array elements by the length of the array.
-This means that if the sum of the array elements is not evenly divisible by the length of the array, the calculated "average" will be incorrect.
+At first glance, the comment for the calculateArea() method seems accurate and helpful.
+ However, the method actually has a bug - it does not handle negative radii correctly, and will return a NaN (Not a Number) result.
  */
 public class MisleadingComments {
-    // This function calculates the average of the given array
-    public double calculateAverage(int[] array) {
-        int sum = 0;
-        for (int num : array) {
-            sum += num;
+
+    //This method calculates the area of a circle.
+    public double calculateArea(double radius) {
+        double area = Math.PI * radius * radius;
+        return area;
+    }
+
+ /*
+  The comment is misleading because it implies that the method is doing what it's supposed to do, when in fact it has a bug that can cause unexpected results.
+  The comment should be updated to reflect the bug and explain the limitations of the method.
+
+ This method calculates the area of a circle.
+ Note that this method does not handle negative radii correctly,
+ and will return NaN (Not a Number) if the radius is negative.
+  */
+    public double calculateAreaFixed(double radius) {
+        if (radius < 0) {
+            return Double.NaN;
         }
-        return (double) sum / array.length; // divide by the number of elements in the array to get the average
+        double area = Math.PI * radius * radius;
+        return area;
     }
 }
+
