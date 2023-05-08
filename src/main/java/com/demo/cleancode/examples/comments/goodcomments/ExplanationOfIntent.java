54d75c66acefd697fc653d29b24de2df78da7cbe
[Thomas-Alexandru Borbeli] <thomas-alexandru.borbeli@ing.com>
2023-05-08 11:06:36 +0300

    finish comments code examples

--- src/main/java/com/demo/cleancode/examples/comments/goodcomments/ExplanationOfIntent.java
+++ src/main/java/com/demo/cleancode/examples/comments/goodcomments/ExplanationOfIntent.java
@@ -2,20 +2,39 @@ package com.demo.cleancode.examples.comments.goodcomments;
 
 public class ExplanationOfIntent {
 
-    // We're using a binary search algorithm here because the array is already sorted, which will give us O(log n) performance.
-    public int binarySearch(int[] array, int value) {
+    // We're using a binary search algorithm to search for a user's grade because the array is already sorted
+    public int searchUserGrade(Grade[] grades, String user) {
         int left = 0;
-        int right = array.length - 1;
+        int right = grades.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (array[mid] == value) {
-                return mid;
-            } else if (array[mid] < value) {
+            if (grades[mid].getUser().equals(user)) {
+                return grades[mid].getGrade();
+            } else if (grades[mid].getUser().compareTo(user) < 0) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
         return -1;
+    }
+
+    public class Grade {
+        private String user;
+        private int grade;
+
+        public Grade(String user, int grade) {
+            this.user = user;
+            this.grade = grade;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public int getGrade() {
+            return grade;
+        }
     }
 }
